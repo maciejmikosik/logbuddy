1a2bb8e92bfe24470c0dd9c1d4ed87510fa65d6d
Maciej Mikosik <maciej@mikosik.com>
2016-09-08 16:42:08 +0200

    implemented time logger

--- main/java/org/logbuddy/Build.java
+++ main/java/org/logbuddy/Build.java
@@ -9,6 +9,7 @@ import org.logbuddy.logger.BrowserLogger;
 import org.logbuddy.logger.ConsoleLogger;
 import org.logbuddy.logger.StackTraceLogger;
 import org.logbuddy.logger.ThreadLogger;
+import org.logbuddy.logger.TimeLogger;
 import org.logbuddy.renderer.HtmlRenderer;
 import org.logbuddy.renderer.TextRenderer;
 
@@ -16,6 +17,7 @@ public class Build {
   public List<Class<?>> loggers = asList(
       StackTraceLogger.class,
       ThreadLogger.class,
+      TimeLogger.class,
       ConsoleLogger.class,
       BrowserLogger.class);
   public List<Class<?>> renderers = asList(
---
+++ test/java/org/logbuddy/logger/TestTimeLogger.java
@@ -0,0 +1,69 @@
+package org.logbuddy.logger;
+
+import static org.logbuddy.logger.TimeLogger.time;
+import static org.logbuddy.model.Property.property;
+import static org.testory.Testory.given;
+import static org.testory.Testory.givenTest;
+import static org.testory.Testory.thenCalled;
+import static org.testory.Testory.thenReturned;
+import static org.testory.Testory.thenThrown;
+import static org.testory.Testory.when;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.logbuddy.LogBuddyException;
+import org.logbuddy.Logger;
+
+public class TestTimeLogger {
+  private Logger logger, timeLogger;
+  private Object model;
+  private Instant instant;
+  private ZoneOffset zone;
+  private Clock clock;
+
+  @Before
+  public void before() {
+    given(instant = Instant.ofEpochMilli(0));
+    given(zone = ZoneOffset.UTC);
+    givenTest(this);
+  }
+
+  @Test
+  public void logs_time_using_clock() {
+    given(instant = Instant.ofEpochMilli(0));
+    given(zone = ZoneOffset.UTC);
+    given(timeLogger = time(Clock.fixed(instant, zone), logger));
+    when(() -> timeLogger.log(model));
+    thenReturned();
+    thenCalled(logger).log(property(ZonedDateTime.ofInstant(instant, zone), model));
+  }
+
+  @Test
+  public void logs_time_using_clock_timezone() {
+    given(instant = Instant.ofEpochMilli(0));
+    given(zone = ZoneOffset.ofHours(2));
+    given(timeLogger = time(Clock.fixed(instant, zone), logger));
+    when(() -> timeLogger.log(model));
+    thenReturned();
+    thenCalled(logger).log(property(ZonedDateTime.ofInstant(instant, zone), model));
+  }
+
+  @Test
+  public void clock_cannot_be_null() {
+    given(clock = null);
+    when(() -> time(clock, logger));
+    thenThrown(LogBuddyException.class);
+  }
+
+  @Test
+  public void logger_cannot_be_null() {
+    given(logger = null);
+    when(() -> time(clock, logger));
+    thenThrown(LogBuddyException.class);
+  }
+}
