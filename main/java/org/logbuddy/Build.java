2bd879b7ce1f6e729577246b27c741367b7c7392
Maciej Mikosik <maciej@mikosik.com>
2016-09-08 15:51:41 +0200

    implemented browser logger

--- main/java/org/logbuddy/Build.java
+++ main/java/org/logbuddy/Build.java
@@ -5,13 +5,15 @@ import static java.util.Arrays.asList;
 import java.util.List;
 
 import org.logbuddy.decorator.LoggingDecorator;
+import org.logbuddy.logger.BrowserLogger;
 import org.logbuddy.logger.ConsoleLogger;
 import org.logbuddy.renderer.HtmlRenderer;
 import org.logbuddy.renderer.TextRenderer;
 
 public class Build {
   public List<Class<?>> loggers = asList(
-      ConsoleLogger.class);
+      ConsoleLogger.class,
+      BrowserLogger.class);
   public List<Class<?>> renderers = asList(
       TextRenderer.class,
       HtmlRenderer.class);
