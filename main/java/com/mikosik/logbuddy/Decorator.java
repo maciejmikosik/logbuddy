3e592209880764ff043dc4803c93f90eb8532108
Maciej Mikosik <maciej@mikosik.com>
2016-09-08 17:27:16 +0200

    deleted old api

--- main/java/com/mikosik/logbuddy/Decorator.java
+++
@@ -1,5 +0,0 @@
-package com.mikosik.logbuddy;
-
-public interface Decorator {
-  <T> T decorate(T original);
-}
--- main/java/com/mikosik/logbuddy/logger/NoLogger.java
+++
@@ -1,7 +0,0 @@
-package com.mikosik.logbuddy.logger;
-
-import com.mikosik.logbuddy.Logger;
-
-public class NoLogger implements Logger {
-  public void log(String message) {}
-}
